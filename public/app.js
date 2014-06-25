e99e79fda29ec4a2cd53d3d9a8a02ef958ac3829
Troy Nichols <tnichols@onsitehd.com>
2014-06-25 12:52:40 -0500

    Add bootstrap

---
+++ public/app.js
@@ -0,0 +1,17 @@
+angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
+  .config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'MainCtrl'
+      })
+      .when('/shows/:id', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/login', {
+
+      })
+  });
